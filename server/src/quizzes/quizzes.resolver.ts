7fc3bdeb7b9ed37ee97f144c332954a377cf1029
ehababdelmawla <eabdo1474@gmail.com>
2023-11-26 18:16:08 +0200

    add More Mutation

--- server/src/quizzes/quizzes.resolver.ts
+++ server/src/quizzes/quizzes.resolver.ts
@@ -11,11 +11,40 @@ import { Quiz } from './quiz.model';
 import { QuizzesService } from './quizzes.service';
 import { Question } from './models/quesion.model';
 import { QuestionInput } from './InputsType/question.input';
+import { QuizInput } from './InputsType/quiz.input';
 
 @Resolver((of) => Quiz)
 export class QuizzesResolver {
   constructor(private readonly quizzesService: QuizzesService) {}
 
+  @Mutation((_) => Quiz)
+  async addQuiz(@Args('quiz') quiz: QuizInput) {
+    return this.quizzesService.addQuiz(quiz);
+  }
+
+  @Mutation((_) => Quiz)
+  async updateQuiz(@Args('quiz') quiz: QuizInput) {
+    return this.quizzesService.updateQuiz(quiz);
+  }
+
+  @Mutation((_) => Quiz)
+  async toggleShuffleAnswers(
+    @Args({ name: 'quizId', type: () => Int }) quizId: number,
+  ) {
+    return this.quizzesService.toggleShuffleAnswers(quizId);
+  }
+
+  @Mutation((_) => Quiz)
+  async toggleShuffleQuestions(
+    @Args({ name: 'quizId', type: () => Int }) quizId: number,
+  ) {
+    return this.quizzesService.toggleShuffleQuestions(quizId);
+  }
+
+  @Mutation((_) => Quiz)
+  async removeQuiz(@Args({ name: 'quizId', type: () => Int }) quizId: number) {
+    return this.quizzesService.deleteQuiz(quizId);
+  }
   @Query((_) => [Quiz])
   async quizzes() {
     return this.quizzesService.findAll();
@@ -25,13 +54,6 @@ export class QuizzesResolver {
   async questions(@Parent() quiz: Quiz) {
     const { id } = quiz;
     return this.quizzesService.questions(id);
-  }
-
-  @Mutation((_) => Quiz)
-  async toggleShuffleAnswers(
-    @Args({ name: 'quizId', type: () => Int }) quizId: number,
-  ) {
-    return this.quizzesService.toggleShuffleAnswers(quizId);
   }
 
   @Mutation((_) => Question)
--- server/src/quizzes/quizzes.service.ts
+++ server/src/quizzes/quizzes.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 
 import * as quizzes from './quizzes-db.json';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class QuizzesService {
+  addQuiz(quiz) {
+    const newQuiz = {
+      ...quiz,
+      questions: [],
+      id: quizzes.length + 1,
+    };
+
+    quizzes.push(newQuiz);
+    return newQuiz;
+  }
+
+  updateQuiz(updateQuiz) {
+    const quiz = quizzes.find((quiz) => quiz.id === updateQuiz.id);
+    if (quiz) return Object.assign(quiz, { ...updateQuiz });
+    return new HttpException('Quiz Not Found', 404);
+  }
+
+  deleteQuiz(quizId) {
+    const quizToRemove = quizzes.find((quiz) => quiz.id === quizId);
+    const indexToRemove = quizzes.indexOf(quizToRemove);
+    quizzes.splice(indexToRemove, 1);
+    return quizToRemove;
+  }
+
   findAll() {
     return quizzes;
   }
@@ -16,6 +41,12 @@ export class QuizzesService {
   toggleShuffleAnswers(quizId) {
     const quiz = quizzes.find((quiz) => quiz.id === quizId);
     quiz.shuffleAnswers = !quiz.shuffleAnswers;
+    return quiz;
+  }
+
+  toggleShuffleQuestions(quizId) {
+    const quiz = quizzes.find((quiz) => quiz.id === quizId);
+    quiz.shuffleQuestions = !quiz.shuffleQuestions;
     return quiz;
   }
 
